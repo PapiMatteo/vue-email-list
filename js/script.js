c51dd2299bfba87ab19cb7d3977e76f65c447376
PapiMatteo <matteopapi.work@gmail.com>
2023-11-21 13:43:07 +0100

    add: exercise completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-
+            mailList: []
+        }
+    },
+    created() {
+        for (let i = 0; i < 10; i++){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    this.mailList.push(resp.data.response)                  
+                })
         }
     }
 }).mount('#app');
